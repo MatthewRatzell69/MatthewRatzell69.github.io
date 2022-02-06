5de837016c72924088f1d6456914d0b3578c339c
Matthew Ratzell <mjr4427@g.rit.edu>
2022-02-06 18:44:58 -0500

    updated meta data
    added some features and hopefully fixed a bug

--- src/appLoader.js
+++ src/appLoader.js
@@ -6,9 +6,8 @@ import "./sw-documentationGuy.js";
 
 
 window.onload = () => {
+
 	console.log("window.onload called");
-	// 1 - do preload here - load fonts, images, additional sounds, etc...
-	// 2 - start up app
 	if (window.location.href.match('index.html')) 
 	{
 		main.init();
