a0e2d534458fc6f7c905c9f9a7a9accafde50249
Matthew Ratzell <mjr4427@g.rit.edu>
2022-02-06 18:57:03 -0500

    bugfix part 2

--- src/appLoader.js
+++ src/appLoader.js
@@ -6,7 +6,14 @@ import "./sw-documentationGuy.js";
 
 
 window.onload = () => {
+	//if on horseplinko .com go to index
+	if (location.href.match('www.horseplinko.com')) 
+	{
+		location.href = 'www.horseplinko.com/index.html'
+		console.log('loading from web');
 
+	}
+	
 	console.log("window.onload called");
 	if (window.location.href.match('index.html')) 
 	{
