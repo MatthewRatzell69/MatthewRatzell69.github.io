11c9603e9cbef2627d6a2b5acbcc5dd0dfb626d3
Matthew Ratzell <mjr4427@g.rit.edu>
2022-02-06 21:59:56 -0500

    fix number 400

--- src/appLoader.js
+++ src/appLoader.js
@@ -9,7 +9,7 @@ window.onload = () => {
 	//method to reload on first load
 	if(!window.location.hash) {
         //setting window location
-        window.location = window.location +'index.html'+'#loaded';
+        window.location = window.location + +'index.html'+'#loaded';
         //using reload() method to reload web page
         window.location.reload();
     }
