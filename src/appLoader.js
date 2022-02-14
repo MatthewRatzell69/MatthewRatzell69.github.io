f9734a85e6baee2edd366e8f64921d39044e37ca
Matthew Ratzell <mjr4427@g.rit.edu>
2022-02-14 16:22:11 -0500

    Hopefully fixed page loading

--- src/appLoader.js
+++ src/appLoader.js
@@ -3,8 +3,26 @@ import "./sw-nav.js";
 import "./sw-footer.js";
 import "./sw-documentationGuy.js";
 
-
+let bool = true;
 window.onload = () => {
+
+	/*
+	if(bool){
+		if (location.href.match('www.horseplinko.com')) 
+		{
+			location.href = '/index.html'
+			console.log('loading from web');
+
+		}
+		bool = false;
+	}
+	*/
+	console.log("window.onload called");
+
+	if (window.location.href.match('index.html')) 
+	{
+		main.init();
+	}
 
 	//method to reload on first load
 	if(!window.location.hash) {
@@ -13,12 +31,5 @@ window.onload = () => {
         //using reload() method to reload web page
         window.location.reload();
     }
-
-	console.log("window.onload called");
-	if (window.location.href.match('index.html')) 
-	{
-		main.init();
-	}
-
 }
 
